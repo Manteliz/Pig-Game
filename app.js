5e977cb5a07243463d0566f83ee7d049e2ff3814
Manteliz <mantasvaist@gmail.com>
2020-01-28 13:10:21 +0200

    Implemented style change when dice rolls 1

--- app.js
+++ app.js
@@ -11,23 +11,34 @@ var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
-// Random number
-var dice = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if(gamePlaying) {
+        // 1. Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-//Display the result
-var diceDom = document.querySelector('.dice');
-diceDom.style.display = 'block';
-diceDom.src = 'dice-' + dice + '.png';
+        //2. Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
 
-//Update the round score IF the rolled number was NOT a 1
-if (dice !== 1) {
-    //Add score
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-} else {
-    //Next player
-    nextPlayer();
-}
+
+        //3. Update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //Next player
+            //nextPlayer();
+            roundScore = 0;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+            activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
+            document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+            document.querySelector('.dice').style.display = 'none';
+        }
+    }    
+});
 
 function init(){
     scores = [0, 0];
@@ -42,11 +53,10 @@ function init(){
     document.getElementById('current-1').textContent = '0';
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
-    /*
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
-    */
+
 }
